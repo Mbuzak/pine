3420ce80a9a5e8537c0f3e37c9b1b7b44958dec1
Mbuzak <marianbuzak@gmail.com>
2025-12-03 13:05:01 +0100

    Another delete redundant code

--- lab2/Zad1/main.cpp
+++ lab2/Zad1/main.cpp
@@ -35,90 +35,49 @@ GLfloat getRandomNumber(float, float);
 Point getPoint();
 
 
-// ---------------------------------------
 void DisplayScene() {
-	// ---------------------------
-	// Etap (5) rendering
-	// ---------------------------
 	glClear( GL_COLOR_BUFFER_BIT );
 
-
-	// Wlaczenie potoku
 	glUseProgram( idProgram );
-
-
-		// Generowanie obiektow na ekranie
 		glBindVertexArray( idVAO );
 		glDrawArrays( GL_POINTS, 0, n );
 		glBindVertexArray( 0 );
-
-
-	// Wylaczanie
 	glUseProgram( 0 );
-
 
 	glutSwapBuffers();
 }
 
-// ---------------------------------------
 void Initialize() {
-	// -------------------------------------------------
-	// Etap (2) przeslanie danych wierzcholk�w do OpenGL
-	// -------------------------------------------------
 	glGenVertexArrays( 1, &idVAO );
 	glBindVertexArray( idVAO );
 
-	// Bufor na wspolrzedne wierzcholkow
 	glGenBuffers( 1, &idVBO_coord );
 	glBindBuffer( GL_ARRAY_BUFFER, idVBO_coord );
 	glBufferData( GL_ARRAY_BUFFER, sizeof( points ) * n, &points.front(), GL_STATIC_DRAW );
 
 	glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, NULL );
 	glEnableVertexAttribArray( 0 );
-
 	glBindVertexArray( 0 );
 
-
-	// ---------------------------------------
-	// Etap (3) stworzenie potoku graficznego
-	// ---------------------------------------
 	idProgram = glCreateProgram();
-
 	glAttachShader( idProgram, LoadShader(GL_VERTEX_SHADER, "vertex.glsl"));
 	glAttachShader( idProgram, LoadShader(GL_FRAGMENT_SHADER, "fragment.glsl"));
-
 	LinkAndValidateProgram( idProgram );
 
-
-
-	// -----------------------------------------
-	// Etap (4) ustawienie maszyny stanow OpenGL
-	// -----------------------------------------
-
 	glEnable(GL_PROGRAM_POINT_SIZE);
-
-	// ustawienie koloru czyszczenia ramki koloru
 	glClearColor( 0.9f, 0.9f, 0.9f, 0.9f );
-
 }
 
-// ---------------------------------------
-// Funkcja wywolywana podczas zmiany rozdzielczosci ekranu
 void Reshape( int width, int height ) {
 	glViewport( 0, 0, width, height );
 }
 
-
-
-// ---------------------------------------------------
 // Funkcja wywolywana podczas wcisniecia klawisza ASCII
 void Keyboard( unsigned char key, int x, int y ) {
     switch(key) {
 		case 27:	// ESC key
-			// opuszczamy glowna petle
 			glutLeaveMainLoop();
 			break;
-
 		case ' ':	// Space key
 			printf("SPACE!\n");
 			// ponowne renderowanie
@@ -128,16 +87,11 @@ void Keyboard( unsigned char key, int x, int y ) {
 }
 
 
-// ---------------------------------------------------
 int main( int argc, char *argv[] ) {
     srand(time(NULL));
 	
 	for ( int i = 0; i < n; i++ )
         points.push_back(getPoint());
-
-	// -----------------------------------------------
-	// Etap (1) utworzynie kontektu i okna aplikacji
-	// -----------------------------------------------
 
 	// GLUT
 	glutInit( &argc, argv );
@@ -149,7 +103,6 @@ int main( int argc, char *argv[] ) {
 	glutDisplayFunc( DisplayScene );
 	glutReshapeFunc( Reshape );
 	glutKeyboardFunc( Keyboard );
-
 
 	// GLEW
 	glewExperimental = GL_TRUE;
@@ -165,9 +118,7 @@ int main( int argc, char *argv[] ) {
 		exit(1);
 	}
 
-	// Inicjalizacja
 	Initialize();
-
 	glutMainLoop();
 
 	// Cleaning
